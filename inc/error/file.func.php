3cfff2c35b5227e3c3668f478c74a34a58ee23d3
Torsten Zuehlsdorff <torsten@p.variablis.de>
2026-06-08 22:58:42 +0200

    Document Exception and parameter requirements of Error\file()

--- inc/error/file.func.php
+++ inc/error/file.func.php
@@ -8,7 +8,18 @@ namespace ImmanentCodeChecker\Error;
   * @param $objFile    - file context the error belongs to
   * @param $intLine    - optional line number the error belongs to
   *
+  * @throws \Exception - if the file context does not provide required fields
+  * @throws \Exception - if the created error does not validate
+  *
   * Register an error for a file context.
+  *
+  * $strCheck and $strMessage must be non-empty strings without leading or
+  * trailing whitespace. $objFile must provide the exploration fields full_path
+  * and relative_path. $intLine may be null. If it is provided, it must be an
+  * integer greater than zero.
+  *
+  * The resulting error is validated by the ERROR_FILE pool before it is stored.
+  * Invalid error data is rejected instead of being stored.
   **/
 function file(string $strCheck,
               string $strMessage,
